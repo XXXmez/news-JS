7a0e0a29b89e1d639daa295e14dcde8edac5ae91
Ruslan Bogdanov <russian.bogd@gmail.com>
2022-07-03 11:15:25 +0300

    feat: transfer start
    At this stage, typescropt has been added to the project. Created tsconfig.json file. ESLint and Webpack are configured to work with TypeScript. The required plugin is included in ESLint. The application is working.

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '25a1390ccf1b47988802228ee55c00c8', // получите свой ключ https://newsapi.org/
         });
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index.ts'),
     mode: 'development',
     module: {
         rules: [
@@ -15,16 +15,22 @@ const baseConfig = {
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.ts', '.tsx', '.js'],
     },
     output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, '../dist'),
+        filename: 'bundle-[hash].js',
+        path: path.resolve(__dirname, './dist'),
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, './src/index.html'),
             filename: 'index.html',
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+            },
         }),
         new CleanWebpackPlugin(),
     ],
